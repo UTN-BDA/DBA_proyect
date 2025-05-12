b4ba2a2429d8c60dfc9533aa76c8472d553f1078
Matt <86981471+MattGeldes@users.noreply.github.com>
2025-05-11 21:27:37 -0300

    se agregan servicios (a completar)

---
+++ app/services/__init__.py
@@ -0,0 +1,1 @@
+from .ingresos import IngresosServicios
---
+++ app/services/ingresos.py
@@ -0,0 +1,15 @@
+from app.models import Ingresos
+#from app.repositories import IngresosRepositorio #to do
+
+class IngresosServicios():
+    def agregar_saldo():
+        pass
+
+    def consultar_ingresos():
+        pass
+
+    def modificar_ingresos():
+        pass
+    
+    def eliminar_ingresos():
+        pass
