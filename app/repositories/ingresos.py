d35de1a05adb8b485bc8da51608911494c2013c7
Matt <86981471+MattGeldes@users.noreply.github.com>
2025-05-11 22:13:22 -0300

    se agrega repositories (a completar)

---
+++ app/repositories/ingresos.py
@@ -0,0 +1,24 @@
+from app import db
+from app .models import Ingresos
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+class IngresosRepositorios:
+    def agregar_saldo(self, ingreso: Ingresos) ->Ingresos:
+        try:
+            db.session.add(ingreso)
+            db.session.commit()
+            return ingreso
+        except IntegrityError as e:
+            db.session.rollback()
+            raise e
+        
+#acá va delete
+    def eliminar_ingresos(self,...): #investigar 
+        pass
+
+    def consultar_ingresos(self,..):
+        pass
+
+    def modificar_ingresos(self, ...):
+        pass
+
